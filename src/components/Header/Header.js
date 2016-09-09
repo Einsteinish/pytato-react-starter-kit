432728d00403daeff9a7fea4869cca2cadbfe141
bogotobogo <k@bogotobogo.com>
2016-09-08 23:44:49 -0700

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Header.css';
+import Link from '../Link';
+import Navigation from '../Navigation';
+import logoUrl from './logo-small.png';
+
+function Header() {
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <Navigation className={s.nav} />
+        <Link className={s.brand} to="/">
+          <img src={logoUrl} width="38" height="38" alt="React" />
+          <span className={s.brandTxt}>Pytato</span>
+        </Link>
+        <div className={s.banner}>
+          <h1 className={s.bannerTitle}>React</h1>
+          <p className={s.bannerDesc}>Complex web apps made easy!</p>
+        </div>
+
+        <div className="g-person" data-width="1" data-href="//plus.google.com/111664369941456137911" data-rel="author">
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default withStyles(s)(Header);
